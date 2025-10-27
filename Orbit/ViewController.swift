62017c09f65b7c57aa137180f80ca211cfd2277e
Evgenij Ovchinnikov <timlamb146@gmail.com>
2025-10-27 17:53:22 +0500

    base init project

---
+++ Orbit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Orbit
+//
+//  Created by Евгений Овчинников on 27.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
